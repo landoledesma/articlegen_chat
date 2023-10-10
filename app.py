92e0c73ed0014ee7def5bca4badfb75b1a153fe3
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-10 15:24:07 -0600

    creando la funcion para generar docx

--- app.py
+++ app.py
@@ -33,3 +33,16 @@ if src_original_url:
     print(f"Original URL for query '{query}': {src_original_url}")
 else:
     print("No se pudo traer imagens lo siento")
+
+def create_docx(user_input,paragraph,image_input):
+    doc = Document()
+    doc.add_heading(user_input)
+    doc.add_paragraph(paragraph)
+
+    doc.add_heading('Image',level=1)
+    image_stream = io.BytesIO()
+    image_input.save(image_stream,format='PNG')
+    image_input.seek(0)
+    doc.add_picture(image_stream,width=Inches(4))
+
+    return doc
