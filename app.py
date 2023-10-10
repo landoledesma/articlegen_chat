678034790f151f9393982646902e702784658ff4
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-10 15:56:09 -0600

    definiendo el frontend

--- app.py
+++ app.py
@@ -46,3 +46,61 @@ def create_docx(user_input,paragraph,image_input):
     doc.add_picture(image_stream,width=Inches(4))
 
     return doc
+
+st.set_page_config(layout="wide")
+
+def main():
+    st.title("Aplicación Generadora de Artículoscon AI")
+
+    entrada_usuario = st.text_input("¡Por favor, introduce la idea/tema para el artículo que deseas generar!")
+
+    entrada_imagen = st.text_input("¡Por favor, introduce el tema de la imagen que deseas obtener!")
+
+    if len(entrada_usuario) > 0 and len(entrada_imagen) > 0:
+
+        col1, col2, col3 = st.columns([1,2,1])
+
+        with col1:
+            st.subheader("Contenido Generado con inteligencia artifical")
+            st.write("El tema del artículo es: " + entrada_usuario)
+            st.write("La imagen del artículo es: " + entrada_imagen)
+            plantilla_prompt = """Eres un experto en marketing digital y SEO y tu tarea es escribir un artículo
+              sobre el tema proporcionado: {entrada_usuario}. El artículo no debe superar las 800 palabras.
+              El artículo no debe ser muy largo.
+            """
+            llamada_llm = cargar_llm(max_tokens=800, plantilla_prompt=plantilla_prompt)
+            print(llamada_llm)
+            resultado = llamada_llm(entrada_usuario)
+            if len(resultado) > 0:
+                st.info("¡Tu artículo ha sido generado con éxito!")
+                st.write(resultado)
+            else:
+                st.error("¡No se pudo generar tu artículo!")
+
+        with col2:
+            st.subheader("Imagen Obtenida")
+            url_imagen = obtener_url_original(entrada_imagen)
+            st.image(url_imagen)
+
+
+        with col3:
+            st.subheader("Articulo final")
+            image_input = "temp_image.jpg"
+            doc = create_word_docx(user_input, result['text'], Image.open(image_input))
+
+            # Save the Word document to a BytesIO buffer
+            doc_buffer = io.BytesIO()
+            doc.save(doc_buffer)
+            doc_buffer.seek(0)
+
+            # Prepare the download link
+            st.download_button(
+                label='Download Word Document',
+                data=doc_buffer,
+                file_name='document.docx',
+                mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            )
+
+
+if __name__ == "__main__":
+    main()
